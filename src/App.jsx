3751a01e18b633eb0a0ecd908fbcad1683b3616a
Sheriff <sheriffaliuyamah82@gmail.com>
2025-10-14 16:11:40 -0700

    State initializer functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Counter from './counter'
+import Dumbo from './Dumbo'
 
 function App() {
 
   return (
     <>
       <h1>state demo</h1>
-      <Counter/>
+      <Dumbo/>
+      {/* <Counter/> */}
     </>
   )
 }
---
+++ src/Dumbo.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+function generateGameBoard() {
+    console.log('making game board')
+    return Array(5000)
+}
+
+export default function Dumbo() {
+    const [board, setBoard] = useState(generateGameBoard)
+    return (
+        <button onClick={() => setBoard('hello')}>Generate game board</button>
+    )
+}
+
