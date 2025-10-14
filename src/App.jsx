da35564ee6cbfe26a264c3832d840895a1acf960
Sheriff <sheriffaliuyamah82@gmail.com>
2025-10-14 15:46:56 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Counter from './counter'
+
+function App() {
+
+  return (
+    <>
+      <h1>state demo</h1>
+      <Counter/>
+    </>
+  )
+}
+
+export default App
---
+++ src/counter.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react" 
+
+export default function Counter() {
+  const [count, setCount] = useState(0)
+  const addOne = () => {
+    console.log(count)
+    setCount(c => c + 1)
+  }
+  const addThree = () => {
+    setCount(c => c + 1)
+    setCount(c => c + 1)
+    setCount(c => c + 1)
+  }
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={addOne}>+1</button>
+      <button onClick={addThree}>+3</button>
+    </div>
+  )
+}
