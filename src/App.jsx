e60ebf2ee957bbac4aef0af97ce26bde08ab91f6
Sheriff <sheriffaliuyamah82@gmail.com>
2025-10-14 19:05:37 -0700

    Added EmojiClicker

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import './App.css'
 import Counter from './counter'
 import Dumbo from './Dumbo'
 import ScoreKeeper from './ScoreKeeper'
+import EmojiClicker from './EmojiClicker'
 
 function App() {
 
   return (
     <>
       <h1>state demo</h1>
-      <ScoreKeeper/>
+      <EmojiClicker/>
+      {/* <ScoreKeeper/> */}
       {/* <Dumbo/> */}
-      <Counter/>
+      {/* <Counter/> */}
     </>
   )
 }
---
+++ src/EmojiClicker.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import { v4 as uuid } from "uuid"
+
+function randomEmoji() {
+    const choices = ["🤯", "😳", "😝", "🙁"]
+    return choices[Math.floor(Math.random()*choices.length)]
+}
+
+export default function EmojiClicker() {
+    const [emojis, setEmojis] = useState([{id: uuid(), emoji: "😭"}])
+    const addEmoji = () => {
+        setEmojis(oldEmojis => {
+           return [...oldEmojis, {id: uuid(), emoji: randomEmoji()}]
+        })
+    }
+    const deleteEmoji = (id) => {
+        setEmojis(prevEmojis => {
+            return prevEmojis.filter(e => e.id !== id)
+        })
+    }
+    return (
+        <div>
+            {emojis.map((e, i) => (
+                <span onClick={() => deleteEmoji(e.id)} key={e.id} style={{ fontSize: "4rem" }}>{e.emoji}</span>
+            ))}
+            <button onClick={addEmoji}>Add Emoji</button>
+        </div>
+    )
+}
